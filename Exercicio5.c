aaee53268dee90e05993a20f9eaa5a6f1e6c7ab2
Gabriel Ritter <99975124+RitterGabriel@users.noreply.github.com>
2025-04-25 19:37:42 -0300

    Add files via upload

---
+++ Exercicio5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	float notas[5] = {7, 8, 9.5, 9.9, 5.2};
+	printf("Exibindo os Valores do Vetores do Vetor \n\n");
+	printf("notas[0] = %.1f\n", notas[0]);
+	printf("notas[1] = %.1f\n", notas[1]);
+	printf("notas[2] = %.1f\n", notas[2]);
+	printf("notas[3] = %.1f\n", notas[3]);
+	printf("notas[4] = %.1f\n", notas[4]);	
+	return 0;
+}
+
